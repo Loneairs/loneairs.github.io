7b1b003b83ea19c5d3e5782843cdf79b0682d33e
Adrian404 <3285154019@qq.com>
2021-06-28 20:58:51 +0800

    test1

---
+++ res/index.js
@@ -0,0 +1,8 @@
+function GetDocWidth() {
+	var width=document.documentElement.clientWidth || document.body.clientWidth;
+	return width;
+}
+function GetDocHeight() {
+	var height=document.documentElement.clientHeight || document.body.clientHeight;
+	return height;
+}
